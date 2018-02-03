88bde685120f0781a4d1913ef700cdf1eca8042c
Kwanho Kim <kimkh415@gmail.com>
2018-02-03 03:10:30 -0500

    commit to merge

--- parse.py
+++ parse.py
@@ -1,5 +1,5 @@
 import csv
-
+import datetime
 
 def read_data(file_name, list):
     with open(file_name) as file:
@@ -17,6 +17,13 @@ def cross_reference(personal, ref):
                 return False
     return True
 
+def time(d):
+    today = datetime.datetime.now().date()
+    d = d.split("/")
+    date = datetime.date(int(d[2]), int(d[0]), int(d[1]))
+    days = (today-date).days
+    print(days)
+    return days
 
 # Data structure for each file we are keeping track of
 # [[list of symptoms], [list of allergy], age, weight, eat, severity, date]]
@@ -27,6 +34,9 @@ personal = personal[0]
 # [[type, [list of details], date], ...]
 eob = []
 read_data('eob.csv', eob)
+
+database = []
+read_data('database.csv', database)
 
 # [[drug name, [list of interacting drugs], [list of side effects]], ...]
 interaction = []
@@ -39,16 +49,37 @@ avoid = []
 for allergy in personal[1].split(", "):
     avoid.append(allergy)
 
-current_meds = []
-for record in eob:
-    if record[0] == "DRUGS":
-        current_meds.append(record[1])
+database = database + eob
 
-for my_med in current_meds:
-    for not_with_my_med in interaction:
-        if my_med in not_with_my_med[1].split(", "):
-            avoid.append(not_with_my_med[0])
+all_meds = []
+all_meds_dates = []
+all_keywords = []
+all_keywords_dates = []
+
+for record in database:
+    if record[0] == "DRUGS":
+        all_meds.append(record[1])
+        all_meds_dates.append(record[2])
+    elif record[0] == "IMAGING" or record[0] == "PHYSICIAN":
+        all_keywords.append(record[1])
+        all_keywords_dates.append(record[2])
+"""
+for i, my_med in enumerate(all_meds):
+    for row in interaction:
+        if my_med in row[1].split(", ") and time(all_meds_dates[i]) < 7:
+            avoid.append(row[0])
+
+for i, keyword in enumerate(all_keywords):
+    for row in interaction:
+        effects = []
+        side_effects = row[2].split(", ")
+        for effect in side_effects:
+            if keyword in effect.split(" ") and time(all_meds_dates[i]) < 7:
+                avoid.append(row[0])
+"""
+
 
 
 
 avoid = list(set(avoid))
+print(avoid)
